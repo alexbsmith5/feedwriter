5e995b87863cc5200ca50d757fdc34427d2d2a00
Alex Smith <alexbsmith52@gmail.com>
2026-06-12 16:34:12 -0500

    Apple Podcasts channel and episode tags (#1)

---
+++ src/feedwriter/podcast_feed.py
@@ -0,0 +1,175 @@
+import xml.etree.ElementTree as ET
+
+class PodcastFeed:
+    # empty constructor
+    def __init__(self):
+        self.root = ET.Element("rss", { "version":"2.0", "xmlns:itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd" })
+        self.channel = ET.SubElement(self.root, "channel")
+        self.tree = ET.ElementTree(self.root)
+        self.item = []
+
+    # set title
+    def title(self, title: str):
+        ET.SubElement(self.channel, "title").text = title
+
+    # set description
+    def description(self, description: str):
+        ET.SubElement(self.channel, "description").text = description
+
+    # set image
+    def image(self, url):
+        ET.SubElement(self.channel, "itunes:image", href=url).text
+
+    # set language
+    def language(self, language: str):
+        ET.SubElement(self.channel, "language").text = language
+
+    # set category
+    # TODO: add support for subcategories
+    def category(self, category: str):
+        ET.SubElement(self.channel, "itunes:category", text=category).text
+
+    # set explicit
+    def explicit(self, explicit: bool):
+        if explicit:
+            text = "true"
+        else:
+            text = "false"
+        ET.SubElement(self.channel, "itunes:explicit").text = text
+
+    # set author
+    def author(self, author: str):
+        ET.SubElement(self.channel, "itunes:author").text = author
+
+    # set link
+    def link(self, url: str):
+        ET.SubElement(self.channel, "link").text = url
+
+    # set itunes title
+    # specific name on apple podcasts
+    def itunes_title(self, title: str):
+        ET.SubElement(self.channel, "itunes:title").text = title
+
+    # TODO: <itunes:type>
+    #  if serial type is chosen, <itunes:episode> tag must be specified for each post
+
+    # set copyright
+    def copyright(self, copyright: str):
+        ET.SubElement(self.channel, "copyright").text = copyright
+
+    # set url of new feed
+    def feed_url_new(self, url: str):
+        ET.SubElement(self.channel, "itunes:new-feed-url").text = url
+
+    # set block (removes podcast from apple directory)
+    # don't use if not trying to block
+    def block(self):
+        ET.SubElement(self.channel, "itunes:block").text = "Yes"
+
+    # set complete, no more new episodes will be added
+    def complete(self):
+        ET.SubElement(self.channel, "itunes:complete").text = "Yes"
+
+    # set token to verify podcast with apple podcasts
+    def verify(self, token: str):
+        ET.SubElement(self.channel, "itunes:applepodcastsverify").text = token
+
+    # set name of rss-generator
+    def generator(self, url: str):
+        ET.SubElement(self.channel, "generator").text = url
+
+    # find post index given title
+    def get_post_index(self, title: str) -> int:
+        index = 0
+        for item in self.root.findall(".//item"):
+            if item.find('title').text == title:
+                return index
+            index += 1
+        return -1 # if title not found return -1 index
+
+    # add post enclosure to post from index
+    def post_enclosure(self, url: str, file_size: str, type: str, index: int = -1):
+        ET.SubElement(self.item[index], "enclosure", url=url, length=file_size, type=type)
+
+    # add post enclosure
+    def post_guid(self, guid: str, index: int = -1):
+        ET.SubElement(self.item[index], "guid").text = guid
+
+    # add post date
+    # date format according to RFC 2822 specification
+    def post_date(self, date: str, index: int = -1):
+        # TODO: better way for user to input date
+        #  option to input date object?
+        ET.SubElement(self.item[index], "pubdate").text = date
+
+    # add post description
+    def post_description(self, description: str, index: int = -1):
+        ET.SubElement(self.item[index], "description").text = description
+
+    # add post duration (in seconds)
+    def post_duration(self, seconds: int, index: int = -1):
+        # TODO: add more formats?
+        #  itunes docs mention other formats (recommended to stay in seconds
+        ET.SubElement(self.item[index], "itunes:duration").text = str(seconds)
+
+    # add post url
+    # use when post has corresponsing webpage
+    def post_link(self, url: str, index: int = -1):
+        ET.SubElement(self.item[index], "link").text = url
+
+    # add post image url
+    def post_image(self, url: str, index: int = -1):
+        ET.SubElement(self.item[index], "itunes:image", href=url).text
+
+    # add explicit tag
+    def post_explicit(self, explicit: bool, index: int = -1):
+        if explicit:
+            text = "true"
+        else:
+            text = "false"
+        ET.SubElement(self.item[index], "itunes:explicit").text = text
+
+    # add itunes title
+    # specific episode name for apple podcasts
+    def post_itunes_title(self, title: str, index: int = -1):
+        ET.SubElement(self.item[index], "itunes:title").text = title
+
+    # add post number
+    # not needed for episodic shows (default)
+    def post_episode_number(self, num: int, index: int = -1):
+        ET.SubElement(self.item[index], "itunes:episode").text = str(num)
+
+    # add season number
+    # not needed for episodic shows (default)
+    def post_season_number(self, num: int, index: int = -1):
+        ET.SubElement(self.item[index], "itunes:season").text = str(num)
+
+    # add post type
+    # options: full, trailer, bonus
+    def post_type(self, type: str, index: int = -1):
+        ET.SubElement(self.item[index], "itunes:episodeType").text = type
+
+    # TODO: podcast namespace tags
+
+    # add post block
+    # add post block (removes episode from apple directory
+    # don't use if not trying to block
+    def post_block(self, index: int = -1):
+        ET.SubElement(self.item[index], "itunes:block").text = "Yes"
+
+    # add post with title
+    def new_post(self, title: str):
+        self.item.append(ET.SubElement(self.channel, "item"))
+        ET.SubElement(self.item[-1], "title").text = title
+
+    # add post with required tags
+    def new_post_required(self, title: str, url: str, file_size: str, type: str, guid: str):
+        self.item.append(ET.SubElement(self.channel, "item"))
+        ET.SubElement(self.item[-1], "title").text = title
+        self.post_enclosure(url, file_size, type)
+        self.post_guid(guid)
+
+    # write tree to xml file
+    def write(self, filename):
+        self.tree = ET.ElementTree(self.root)
+        self.tree.write(filename, xml_declaration=True, encoding="UTF-8")
---
+++ tests/podcast_feed_test.py
@@ -0,0 +1,60 @@
+from src.feedwriter import podcast_feed
+
+def main():
+    feed = podcast_feed.PodcastFeed()
+
+    # required channel tags
+    feed.title("Test RSS Feed")
+    feed.description("Description for \"Test RSS Feed\".")
+    feed.image("https://website.com/image.jpg")
+    feed.language("eng")
+    feed.category("Comedy")
+    feed.explicit(True)
+
+    # recommended tags
+    feed.author("Podcast Author")
+    feed.link("https://website.com")
+
+    # situational tags
+    feed.itunes_title("Test RSS Feed")
+    feed.copyright("2026 Podcast Author")
+    feed.feed_url_new("https://website1.com/feed.xml")
+    feed.block()
+    feed.complete()
+    feed.verify("token")
+    feed.generator("https://github.com/alexbsmith5/feedwriter")
+
+    # add new posts at once
+    for i in range(5):
+        feed.new_post_required(f"Post {i}", f"https://website.com/post-{i}.mp3", f"{5650880+i}", "audio/mpeg", str(i))
+
+    # add additional information to posts
+    post_3 = feed.get_post_index("Post 3")
+    if post_3 != -1: # check if get_post_index found valid index
+        feed.post_guid("33", 3)
+
+    # add new posts incrementally
+    feed.new_post("Post 5")
+    # required tags
+    feed.post_enclosure("https://website.com/post-5.mp3", "5650880", "audio/mpeg", 5)
+    feed.post_guid("5")
+    # recommended tags
+    feed.post_date("Sat, 01 Apr 2023 19:00:00 GMT")
+    feed.post_description("Description for post 5")
+    feed.post_duration(6351)
+    feed.post_link("https://website.com/post-5-info.html")
+    feed.post_image("https://website.com/post-5.jpg")
+    feed.post_explicit(True)
+    # situational tags
+    feed.post_itunes_title("Post 5")
+    feed.post_episode_number(5)
+    feed.post_season_number(1)
+    feed.post_type("full")
+    feed.post_block()
+
+
+    # write feed
+    feed.write("test_feed.xml")
+
+if __name__ == "__main__":
+    main()

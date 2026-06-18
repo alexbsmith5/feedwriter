03a98505ae9932f8ec30974e76ce69bf701783f1
alexbsmith5 <alexbsmith52@gmail.com>
2026-06-18 14:55:39 -0400

    Move boilerplate to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+import pytest
+
+from feedwriter.podcast_feed import PodcastFeed
+
+# create base rss feed
+@pytest.fixture
+def base_feed(tmp_path):
+    return PodcastFeed()
+
+# test xml output for text or attrib
+@pytest.fixture
+def assert_xml(tmp_path):
+    def _assert_xml(feed, xpath, expected_text=None, expected_attrib=None):
+        tmp_file = tmp_path / "feed.xml"
+        feed.write(tmp_file)
+
+        tree = ET.parse(tmp_file)
+        root = tree.getroot()
+
+        element = root.find(xpath)
+        assert element is not None, f"Element {xpath} is not found in the XML output."
+
+        if expected_text is not None:
+            assert element.text == expected_text, f"Expected {element.tag} to contain {expected_text}, got {element.text}."
+
+        if expected_attrib is not None:
+            for key, value in expected_attrib.items():
+                assert element.get(key) == value, f"Expected {key} to contain {value}, got {element.get(key)}."
+
+    return _assert_xml
--- tests/podcast_feed_test.py
+++ tests/podcast_feed_test.py
@@ -1,25 +1,3 @@
-import xml.etree.ElementTree as ET
-import pytest
-
-from feedwriter.podcast_feed import PodcastFeed
-
-# return xml root
-@pytest.fixture
-def get_root(tmp_path):
-
-    def generate(feed_object):
-        feed_file = tmp_path / "feed.xml"
-        feed_object.write(feed_file)
-        return ET.parse(feed_file).getroot()
-
-    return generate
-
-def test_title(get_root):
-    feed = PodcastFeed()
-
-    feed.title(test_title)
-    test_title = "Testing Title"
-
-    root = get_root(feed)
-
-    assert root.find("./channel/title").text == test_title
+def test_title(base_feed, assert_xml):
+    base_feed.title("Testing Title")
+    assert_xml(base_feed, "./channel/title", expected_text="Testing Title")

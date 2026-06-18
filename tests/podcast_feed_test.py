3afca52c28f910f258ac485acdb258458d28b319
alexbsmith5 <alexbsmith52@gmail.com>
2026-06-18 15:17:31 -0400

    Use pytest parametrize to make test skeleton

--- tests/podcast_feed_test.py
+++ tests/podcast_feed_test.py
@@ -1,3 +1,20 @@
-def test_title(base_feed, assert_xml):
-    base_feed.title("Testing Title")
-    assert_xml(base_feed, "./channel/title", expected_text="Testing Title")
+import pytest
+
+@pytest.mark.parametrize(
+        "func_name, func_kwargs, xpath, expected_text, expected_attrib",
+        [
+            (
+                "title",
+                {"title": "Testing Title"},
+                "./channel/title",
+                "Testing Title",
+                None
+            ),
+        ]
+)
+
+def test_element(base_feed, assert_xml, func_name, func_kwargs, xpath, expected_text, expected_attrib):
+    mapped_func = getattr(base_feed, func_name)
+    mapped_func(**func_kwargs)
+
+    assert_xml(base_feed, xpath, expected_text=expected_text, expected_attrib=expected_attrib)

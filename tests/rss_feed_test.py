f393e8cbbd64309c1666c9e7a9db3f4cae4d61eb
alexbsmith5 <alexbsmith52@gmail.com>
2026-07-27 20:13:42 -0400

    Fix pytest

--- tests/rss_feed_test.py
+++ tests/rss_feed_test.py
@@ -5,19 +5,16 @@ import pytest
 from feedwriter.rss_feed import RSSFeed
 
 
+# create rss_feed object
 @pytest.fixture
-def rss_feed(tmp_path) -> RSSFeed:
+def rss_feed() -> RSSFeed:
     return RSSFeed()
 
 
-@pytest.fixture
-def assert_xml(
-    tmp_path: Path,
-    rss_feed: RSSFeed,
-    xpath: str,
-    expected_text: str | None = None,
-    expected_attrib: dict[str, str] | None = None,
-):
+# return element given xpath
+def _get_xml_element(
+    rss_feed: RSSFeed, xpath: str, tmp_path: Path
+) -> ET.Element | None:
     # write tmp_file
     tmp_file = tmp_path / "feed.xml"
     rss_feed.write(tmp_file)
@@ -25,23 +22,9 @@ def assert_xml(
     # load tmp_file in memory
     tree = ET.parse(tmp_file)
     root = tree.getroot()
-    ET.dump(root)
 
-    # TODO: return if assertion fails (element is not found)
-    element: ET.Element[str] | None = root.find(xpath)
-    assert element is not None, f"Element {xpath} is not found in the XML output."
-
-    if expected_text is not None:
-        print("Expected text")
-        assert element.text == expected_text, (
-            f"Expected {element.tag} to contain {expected_text}, got {element.text}."
-        )
-
-    if expected_attrib is not None:
-        for attrib, value in expected_attrib.items():
-            assert element.get(attrib) == value, (
-                f"Expected {attrib} to contain {value}, got {element.get(attrib)}."
-            )
+    # return element (if found)
+    return root.find(xpath)
 
 
 @pytest.mark.parametrize(
@@ -51,24 +34,44 @@ def assert_xml(
             "channel_tag",
             {"tag": "title", "content": "Lorem Ipsum"},
             "./channel/title",
-            "lorem ipsum",
+            "Lorem Ipsum",
             None,
-        )
+        ),
+        (
+            "channel_tag",
+            {"tag": "image", "href": "https://example.com/image.jpg"},
+            "./channel/image",
+            None,
+            {"href": "https://example.com/image.jpg"},
+        ),
     ],
 )
 def test_element(
-    assert_xml,
+    tmp_path: Path,
     rss_feed: RSSFeed,
     func_name: str,
     func_kwargs: dict[str, str],
     xpath: str,
     expected_text: str | None,
-    expected_attrib: str | None,
+    expected_attrib: dict[str, str] | None,
 ):
     # run function w/ kwargs
     mapped_func = getattr(rss_feed, func_name)
     mapped_func(**func_kwargs)
-    rss_feed.write("text.xml")
 
-    # test xml
-    assert_xml(rss_feed, xpath, expected_text, expected_attrib)
+    # check if element exists
+    element: ET.Element | None = _get_xml_element(rss_feed, xpath, tmp_path)
+    assert element is not None, f"Element {xpath} is not found in the XML output."
+
+    # check expected content
+    if expected_text is not None:
+        assert element.text == expected_text, (
+            f"Expected {element.tag} to contain {expected_text}, got {element.text}."
+        )
+
+    # check expected attributes
+    if expected_attrib is not None:
+        for attrib, value in expected_attrib.items():
+            assert element.get(attrib) == value, (
+                f"Expected {attrib} to contain {value}, got {element.get(attrib)}."
+            )

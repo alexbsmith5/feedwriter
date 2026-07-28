98fd1258ec3831c7651cba29410882a50d0ae0a2
alexbsmith5 <alexbsmith52@gmail.com>
2026-07-28 10:48:29 -0400

    Move RSSFeed testing logic to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,61 @@
 import xml.etree.ElementTree as ET
+from pathlib import Path
 import pytest
 
-from feedwriter import PodcastFeed
+from feedwriter.rss_feed import RSSFeed
 
 
-# create base rss feed
+# create rss_feed object
 @pytest.fixture
-def base_feed(tmp_path):
-    return PodcastFeed()
+def rss_feed() -> RSSFeed:
+    return RSSFeed()
 
 
-# test xml output for text or attrib
+# return element given xpath
+def _get_xml_element(
+    rss_feed: RSSFeed, xpath: str, tmp_path: Path
+) -> ET.Element | None:
+    # write tmp_file
+    tmp_file = tmp_path / "feed.xml"
+    rss_feed.write(tmp_file)
+
+    # load tmp_file in memory
+    tree = ET.parse(tmp_file)
+    root = tree.getroot()
+
+    # return element (if found)
+    return root.find(xpath)
+
+
 @pytest.fixture
-def assert_xml(tmp_path):
-    def _assert_xml(feed, xpath, expected_text=None, expected_attrib=None):
-        tmp_file = tmp_path / "feed.xml"
-        feed.write(tmp_file)
+def assert_xml(tmp_path: Path):
+    def _assert_xml(
+        rss_feed: RSSFeed,
+        func_name: str,
+        func_kwargs: dict[str, str],
+        xpath: str,
+        expected_text: str | None,
+        expected_attrib: dict[str, str] | None,
+    ):
+        # run function w/ kwargs
+        mapped_func = getattr(rss_feed, func_name)
+        mapped_func(**func_kwargs)
 
-        tree = ET.parse(tmp_file)
-        root = tree.getroot()
-
-        namespaces = {
-            "itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd",
-            "podcast": "https://podcastindex.org/namespace/1.0",
-            "content": "http://purl.org/rss/1.0/modules/content/",
-            "atom": "http://www.w3.org/2005/Atom",
-        }
-
-        element = root.find(xpath, namespaces=namespaces)
+        # check if element exists
+        element: ET.Element | None = _get_xml_element(rss_feed, xpath, tmp_path)
         assert element is not None, f"Element {xpath} is not found in the XML output."
 
+        # check expected content
         if expected_text is not None:
             assert element.text == expected_text, (
                 f"Expected {element.tag} to contain {expected_text}, got {element.text}."
             )
 
+        # check expected attributes
         if expected_attrib is not None:
-            for key, value in expected_attrib.items():
-                assert element.get(key) == value, (
-                    f"Expected {key} to contain {value}, got {element.get(key)}."
+            for attrib, value in expected_attrib.items():
+                assert element.get(attrib) == value, (
+                    f"Expected {attrib} to contain {value}, got {element.get(attrib)}."
                 )
 
     return _assert_xml
--- tests/rss_feed_test.py
+++ tests/rss_feed_test.py
@@ -1,30 +1,6 @@
-import xml.etree.ElementTree as ET
-from pathlib import Path
 import pytest
 
 from feedwriter.rss_feed import RSSFeed
-
-
-# create rss_feed object
-@pytest.fixture
-def rss_feed() -> RSSFeed:
-    return RSSFeed()
-
-
-# return element given xpath
-def _get_xml_element(
-    rss_feed: RSSFeed, xpath: str, tmp_path: Path
-) -> ET.Element | None:
-    # write tmp_file
-    tmp_file = tmp_path / "feed.xml"
-    rss_feed.write(tmp_file)
-
-    # load tmp_file in memory
-    tree = ET.parse(tmp_file)
-    root = tree.getroot()
-
-    # return element (if found)
-    return root.find(xpath)
 
 
 @pytest.mark.parametrize(
@@ -46,32 +22,13 @@ def _get_xml_element(
         ),
     ],
 )
-def test_element(
-    tmp_path: Path,
+def test_function(
     rss_feed: RSSFeed,
+    assert_xml,
     func_name: str,
     func_kwargs: dict[str, str],
     xpath: str,
     expected_text: str | None,
     expected_attrib: dict[str, str] | None,
 ):
-    # run function w/ kwargs
-    mapped_func = getattr(rss_feed, func_name)
-    mapped_func(**func_kwargs)
-
-    # check if element exists
-    element: ET.Element | None = _get_xml_element(rss_feed, xpath, tmp_path)
-    assert element is not None, f"Element {xpath} is not found in the XML output."
-
-    # check expected content
-    if expected_text is not None:
-        assert element.text == expected_text, (
-            f"Expected {element.tag} to contain {expected_text}, got {element.text}."
-        )
-
-    # check expected attributes
-    if expected_attrib is not None:
-        for attrib, value in expected_attrib.items():
-            assert element.get(attrib) == value, (
-                f"Expected {attrib} to contain {value}, got {element.get(attrib)}."
-            )
+    assert_xml(rss_feed, func_name, func_kwargs, xpath, expected_text, expected_attrib)

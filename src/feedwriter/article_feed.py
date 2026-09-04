907fd701320d3ac392ba870e6c134a4f717b7ec1
alexbsmith5 <alexbsmith52@gmail.com>
2026-09-04 16:02:32 -0400

    Remove duplicate functions

--- src/feedwriter/article_feed.py
+++ src/feedwriter/article_feed.py
@@ -8,26 +8,3 @@ class ArticleFeed(Feed):
         """
         namespaces: dict[str, str] = {"xmlns:atom": "http://www.w3.org/2005/Atom"}
         Feed.__init__(self, namespaces)
-
-    def title(self, text):
-        """
-        Set title.
-
-        :param text: title.
-        :type text: string
-        """
-        self.channel_tag("title", text)
-
-    def description(self, text: str, cdata: bool = False):
-        """
-        Set description.
-
-        :param text: description.
-        :type text: string
-        :param cdata: whether or not rich html is included. Ex. ``<a>``, ``<p>``, ``<li>``, etc.
-        :type cdata: bool
-        """
-        if cdata:
-            self.channel_tag("description", f"<![CDATA[ {text} ]]>")
-        else:
-            self.channel_tag("description", text)
